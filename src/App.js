f21e75f8a1ebea3725094d389cd85d82aed9d01b
yayush059 <108602300+yayush059@users.noreply.github.com>
2023-10-29 22:54:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Details from './components/details';
+function App() {
+  return (
+    <div className="App">
+      <Details/>
+    </div>
+  );
+}
+
+export default App;
